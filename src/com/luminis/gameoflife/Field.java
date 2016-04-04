b9bd849c9dadacb10f114d12bd99a1be0c43cc83
Paul Ebeling <paul.ebeling@luminis.eu>
2016-04-04 15:09:40 +0200

    Barebone working console version of Game of Life

--- src/com/luminis/gameoflife/Field.java
+++ src/com/luminis/gameoflife/Field.java
@@ -1,6 +1,7 @@
 package com.luminis.gameoflife;
 
 import java.security.cert.X509Certificate;
+import java.util.Arrays;
 
 /**
  * Created by paul on 04/04/16.
@@ -8,18 +9,13 @@ import java.security.cert.X509Certificate;
 public class Field {
 	private int width, height;
 	private boolean[][] cells;
+
 	Field() {
 		width = 16;
 		height = 10;
-		cells = new boolean[height][width]; // when using boolean[], array is initialized with false
-
-		// Glider pattern, manually put here for now
-		cells[1][3]=true;
-		cells[2][4]=true;
-		cells[3][2]=true;
-		cells[3][3]=true;
-		cells[3][4]=true;
+		cells = new boolean[height][width]; // when using boolean[], array is initialized with false; NB order as for matrices
 	}
+
 	void show() {
 		for(int i = 0; i < height; i++) {
 			for(int j = 0; j < width; j++) {
@@ -27,12 +23,52 @@ public class Field {
 			}
 			System.out.print("\n");
 		}
+		System.out.print("\n");
 	}
-//	int neighbour_count( int cell_x, int cell_y ) {
-//
-//	}
-//	void evolve() {
-//
-//	}
+
+	private int wrapCoordinate(int coordinate, int upperBound) { // implicit lower bound 0
+		coordinate = coordinate % upperBound;
+		return coordinate < 0 ? coordinate + upperBound : coordinate; // this step because of negative remainder in case of negative coordinate
+	}
+
+	boolean get_cell( int cell_x, int cell_y ) {
+		// wrap coordinates, i.e. playing field is a torus
+		return cells[wrapCoordinate(cell_x, height)][wrapCoordinate(cell_y, width)];
+	}
+
+	void set_cell( int cell_x, int cell_y, boolean value ) {
+		// wrap coordinates, i.e. playing field is a torus.
+		cells[wrapCoordinate(cell_x, height)][wrapCoordinate(cell_y, width)] = value;
+	}
+
+	private int numberOfNeighbours( int cell_x, int cell_y ) {
+		int total = 0;
+		for(int i = -1; i <= 1 ; i++) {
+			for(int j = -1; j <= 1; j++) {
+				if ( ( i != 0 || j != 0 ) && get_cell(cell_x+i, cell_y+j) ) total++; // count only neighbours
+			}
+		}
+		return total;
+	}
+
+	void evolve() {
+		Field evolvedField = new Field();
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
+				int numberOfNeighbours = numberOfNeighbours(i, j);
+				boolean newStatus;
+				if ( cells[i][j] ) { // case living cell
+					if ( numberOfNeighbours > 3 ) newStatus = false; // death because of overpopulation
+					else if ( numberOfNeighbours < 2 ) newStatus = false; // death because of under-population
+					else newStatus = true; // stays alive if there are 2 or 3 live neighbours
+				} else { // case dead cell
+					if ( numberOfNeighbours == 3 ) newStatus = true; // birth
+					else newStatus = false; // stays dead
+				}
+				evolvedField.set_cell(i, j, newStatus);
+			}
+		}
+		this.cells = evolvedField.cells;
+	}
 }
 
--- src/com/luminis/gameoflife/Main.java
+++ src/com/luminis/gameoflife/Main.java
@@ -1,8 +1,28 @@
 package com.luminis.gameoflife;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) {
-        Field current_field = new Field();
-        current_field.show();
-	}
+	public static void main(String[] args) throws java.io.IOException {
+		Field current_field = new Field();
+		// Glider pattern
+		current_field.set_cell(1,3,true);
+		current_field.set_cell(2,4,true);
+		current_field.set_cell(3,2,true);
+		current_field.set_cell(3,3,true);
+		current_field.set_cell(3,4,true);
+
+//		current_field.set_cell(9,15,true); // these 2 lines should be a test
+//		System.out.println(current_field.get_cell(-1,-1));
+
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader keyboard = new BufferedReader(input);
+		String line = "";
+		while ( line.equals("") ) {
+			current_field.show();
+			current_field.evolve();
+			line = keyboard.readLine();
+		}
+    }
 }
