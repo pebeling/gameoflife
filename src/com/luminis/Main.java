108f3deb20601dbf4c098a9f99114cfa61dc6d41
Paul Ebeling <paul.ebeling@luminis.eu>
2016-04-04 09:47:32 +0200

    Created repo for my Game of Life implementation

---
+++ src/com/luminis/Main.java
@@ -0,0 +1,16 @@
+package com.luminis;
+
+public class Main {
+
+    class Speelveld {
+        int breedte, hoogte;
+        int[][] cel;
+        Speelveld() {
+            breedte = 80;
+            hoogte = 50;
+        }
+    }
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
