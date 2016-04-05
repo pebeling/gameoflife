ac8b998b601c901658baccc2c55f8ba7b4dd0918
Paul Ebeling <paul.ebeling@luminis.eu>
2016-04-05 14:38:59 +0200

    removed unneeded import

--- src/com/luminis/gameoflife/Main.java
+++ src/com/luminis/gameoflife/Main.java
@@ -2,7 +2,6 @@ package com.luminis.gameoflife;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.StreamCorruptedException;
 
 public class Main {
 	public static void main(String[] args) throws java.io.IOException {
