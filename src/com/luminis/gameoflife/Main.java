758df7b28341e368aa3835f1f15eef1cc6154b95
Paul Ebeling <paul.ebeling@luminis.eu>
2016-04-07 11:16:40 +0200

    Added spaceship and start/stop buttons

--- src/com/luminis/gameoflife/Main.java
+++ src/com/luminis/gameoflife/Main.java
@@ -1,5 +1,7 @@
 package com.luminis.gameoflife;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.*;
 import javafx.stage.*;
 import javafx.scene.*;
@@ -8,6 +10,7 @@ import javafx.scene.control.*;
 import javafx.scene.shape.*;
 import javafx.scene.paint.*;
 import javafx.geometry.*;
+import javafx.util.Duration;
 
 public class Main extends Application {
 	private Field current_field = new Field(20,20);
@@ -29,8 +32,8 @@ public class Main extends Application {
 			for (int j=0; j < current_field.width; j++) {
 				r[i][j] = new Rectangle(20,20);
 				r[i][j].setFill(Color.BLACK);
-				StackPane s = new StackPane(r[i][j]);
-				lifeTile.getChildren().add(s);
+
+				lifeTile.getChildren().add(r[i][j]);
 			}
 		}
 
@@ -45,12 +48,32 @@ public class Main extends Application {
 		evolveButton.setOnAction(e -> evolveButtonClick());
 		evolveButton.setMaxWidth(Double.MAX_VALUE);
 
+
 		Button loadGliderButton = new Button();
-		loadGliderButton.setText("Load Glider");
+		loadGliderButton.setText("Add Glider");
 		loadGliderButton.setOnAction(e -> loadGliderButtonClick());
 		loadGliderButton.setMaxWidth(Double.MAX_VALUE);
 
-		buttons.getChildren().addAll(evolveButton, loadGliderButton);
+		Button loadSpaceshipButton = new Button();
+		loadSpaceshipButton.setText("Add Spaceship");
+		loadSpaceshipButton.setOnAction(e -> loadSpaceshipButtonClick());
+		loadSpaceshipButton.setMaxWidth(Double.MAX_VALUE);
+
+		Button resetFieldButton = new Button();
+		resetFieldButton.setText("Reset");
+		resetFieldButton.setOnAction(e -> resetFieldButtonClick());
+		resetFieldButton.setMaxWidth(Double.MAX_VALUE);
+
+		Button startEvolutionButton = new Button();
+		startEvolutionButton.setText("Start");
+		startEvolutionButton.setMaxWidth(Double.MAX_VALUE);
+
+		Button stopEvolutionButton = new Button();
+		stopEvolutionButton.setText("Stop");
+		stopEvolutionButton.setMaxWidth(Double.MAX_VALUE);
+		stopEvolutionButton.setDisable(true);
+		
+		buttons.getChildren().addAll(loadGliderButton, loadSpaceshipButton, evolveButton, startEvolutionButton, stopEvolutionButton, resetFieldButton);
 
 		BorderPane pane = new BorderPane();
 		pane.setLeft(sPane);
@@ -64,6 +87,35 @@ public class Main extends Application {
 		primaryStage.show();
 		primaryStage.setMaxHeight(primaryStage.getHeight());
 		primaryStage.setMaxWidth(primaryStage.getWidth());
+
+		KeyFrame k = new KeyFrame(Duration.millis(50),
+			e -> {
+				current_field.evolve();
+				updateTileColors();
+
+			} );
+		Timeline t = new Timeline(k);
+		t.setCycleCount(Timeline.INDEFINITE);
+		startEvolutionButton.setOnAction(
+				e -> {
+					evolveButton.setDisable(true);
+					startEvolutionButton.setDisable(true);
+					stopEvolutionButton.setDisable(false);
+					t.play();
+
+				});
+		stopEvolutionButton.setOnAction(
+				e -> {
+					evolveButton.setDisable(false);
+					stopEvolutionButton.setDisable(true);
+					startEvolutionButton.setDisable(false);
+					t.stop();
+				});
+	}
+
+	private void resetFieldButtonClick() {
+		current_field.setField(0, 0, new String[]{});
+		updateTileColors();
 	}
 
 	private void updateTileColors() {
@@ -84,6 +136,16 @@ public class Main extends Application {
 				".O.",
 				"..O",
 				"OOO"
+		});
+		updateTileColors();
+	}
+
+	private void loadSpaceshipButtonClick() {
+		current_field.insertIntoField(6, 3, new String[]{
+				"O..O.",
+				"....O",
+				"O...O",
+				".OOOO"
 		});
 		updateTileColors();
 	}
